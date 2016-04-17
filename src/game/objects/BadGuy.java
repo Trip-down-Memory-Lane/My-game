96746a3a2533e3d3fbd262c15ec9bcc387880c2d
achobanov <alexchobanov@gmail.com>
2016-04-18 00:56:51 +0300

    Improving AI movment

--- src/game/objects/BadGuy.java
+++ src/game/objects/BadGuy.java
@@ -15,7 +15,7 @@ public class BadGuy extends Sprite {
 
     private static int stepX;   // Stores the speed at which BadGuy is moving and occasionaly gives control over direction (positive and negative values).
     private static int stepY;   // Same but for Y axis.
-    private static final int speed = 2;
+    private static int speed = 4;
     // These variables are here for the special case, when Hero and BadGuy both have equal X coordinates, but are separated by wall. This way BadGuy can choose a direction to go around the wall. If not present, BadGuy would just stand bellow Hero.
     private final String[] directions = {"left", "right"};
     private boolean goingRight;
@@ -37,13 +37,13 @@ public class BadGuy extends Sprite {
 
     public void followHero(int heroX, int heroY) {    // moving
         calculateHeading(heroX, heroY);    // get values for 'stepX/stepY'
-        if (!Game.collision.badGuyWallCollide(x + stepX, y + stepY)) {    // if does not collide with wall. 'x + stepX' represents the next coordinates. Basically it checks if the next step (coordinates + step) collides with 'wall'. If not - move, else - skip.
+        if (!wallCollision(x + stepX, y + stepY)) {    // if does not collide with wall. 'x + stepX' represents the next coordinates. Basically it checks if the next step (coordinates + step) collides with 'wall'. If not - move, else - skip.
             x += stepX;
             y += stepY;
         }
         // When we come here - BadGuy collides on the next step.
-        if (!Game.collision.badGuyWallCollide(x + stepX, y) && !deadEnd) {    // checks if collides with wall if moving by X axis. 'deadEnd' is overrule mechanic. See below for more info.
-            if (Game.collision.badGuyWallCollide(x + stepX, y + stepY)) {   // Keep changing 'x' while walking by wall.
+        if (!wallCollision(x + stepX, y) && !deadEnd) {    // checks if collides with wall if moving by X axis. 'deadEnd' is overrule mechanic. See below for more info.
+            if (wallCollision(x + stepX, y + stepY)) {   // Keep changing 'x' while walking by wall.
                 if (Math.abs(heroX - x) <= speed) {
                     chooseDirection('x');    //
                 }
@@ -59,8 +59,8 @@ public class BadGuy extends Sprite {
                 goingDown = false;
                 goingUp = false;
             }
-        } else if (!Game.collision.badGuyWallCollide(x, y + stepY) && !deadEnd) {    // Same as previous. This time we increment or decrement Y values.
-            if (Game.collision.badGuyWallCollide(x + stepX, y + stepY)) {    // Keep doing, until clear of future collisions.
+        } else if (!wallCollision(x, y + stepY) && !deadEnd) {    // Same as previous. This time we increment or decrement Y values.
+            if (wallCollision(x + stepX, y + stepY)) {    // Keep doing, until clear of future collisions.
                 if (Math.abs(heroY - y) <= speed) {
                     chooseDirection('y');    //
                 }
@@ -76,32 +76,52 @@ public class BadGuy extends Sprite {
                 goingLeft = false;    //
                 goingRight = false;    //
             }
-        } else if (!Game.collision.badGuyWallCollide(x - stepX, y)) {    // Initialize 'deadEnd' if we hit the frame border. Stop 'deadEnd' if next step is clear.
+        } else if (!wallCollision(x - stepX, y)) {    // Initialize 'deadEnd' if we hit the frame border. Stop 'deadEnd' if next step is clear.
             x += -stepX;    // while 'deadEnd = true' BadGuy will only move here, because 'deadEnd' is true and all conditions above will be false, thus overruled. When BadGuy moves far enough that its next step is clear in both X and Y axis - 'deadEnd" gets 'false' value and normal movement is restored.
-            deadEnd = Game.collision.badGuyWallCollide(x + stepX, y + stepY);   // evaluate 'deadEnd'
-        } else if (!Game.collision.badGuyWallCollide(x, y - stepY)) {
+            deadEnd = wallCollision(x + stepX, y + stepY);   // evaluate 'deadEnd'
+        } else if (!wallCollision(x, y - stepY)) {
             y += -stepY;
         }
     }
     private void calculateHeading(int heroX, int heroY) {    // Increments or decrements BadGuy(x, y), depending on Hero(x, y)
-        if (Math.abs(heroX - x) <= speed) {
+        if (Math.abs(heroX - x) < speed) {
             stepX = 0;  // Assures no movement on X axis if BadGuy is above or below Hero.
         } else if (heroX < x) {
-            stepX = -speed;
+            if (!(Math.abs(heroY - y) <= speed) && !wallCollision(x, y)) {
+                stepX = -(speed - 1);
+            } else {
+                stepX = -speed;
+            }
             image = Assets.badGuyLeft;
         } else {
-            stepX = speed;
+            if (!(Math.abs(heroY - y) <= speed) && !wallCollision(x, y)) {
+                stepX = speed - 1;
+            } else {
+                stepX = speed;
+            }
             image = Assets.badGuyRight;
         }
-        if (Math.abs(heroY - y) <= speed) {
+        if (Math.abs(heroY - y) < speed) {
             stepY = 0;  // Same as before only for Y axis.
         } else if (heroY < y) {
-            stepY = -speed;
+            if (!(Math.abs(heroX - x) <= speed) && !wallCollision(x, y)) {
+                stepY = -(speed - 1);
+            } else {
+                stepY = -speed;
+            }
             image = Assets.badGuyUp;
         } else {
-            stepY = speed;
+            if (!(Math.abs(heroX - x) <= speed) && !wallCollision(x, y)) {
+                stepY = speed - 1;
+            } else {
+                stepY = speed;
+            }
             image = Assets.badGuyDown;
         }
+    }
+
+    private boolean wallCollision(int x, int y) {
+        return Game.collision.badGuyWallCollide(x, y);
     }
 
     private void chooseDirection(char axis) {    // chooses a direction randomly.
